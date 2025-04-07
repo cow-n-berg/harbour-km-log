11d1723a907e6de5e99e4547334d89785cf83d62
Åke Engelbrektson <eson57@users.noreply.github.com>
2025-04-07 08:43:08 +0200

    Create harbour-km-log-sv.ts
    Add Swedish translation

---
+++ translations/harbour-km-log-sv.ts
@@ -0,0 +1,235 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>GMFS - Geocaching Multi Formula Solver</source>
+        <translation>GMFS - Geocaching Multi Formula Solver</translation>
+    </message>
+    <message>
+        <source>version %1</source>
+        <translation>version %1</translation>
+    </message>
+    <message>
+        <source>created by %1</source>
+        <translation>skapad av %1</translation>
+    </message>
+    <message>
+        <source>Enjoy!</source>
+        <translation>Håll tillgodo!</translation>
+    </message>
+    <message>
+        <source>The source code is available at Github.
+                       &lt;br/&gt;You can contact me for any remarks,
+                       &lt;br/&gt;bugs, feature requests, ideas,...</source>
+        <translation>Källkoden finns på GitHub.
+                       &lt;br/&gt;Du kan kontakta mig för eventuella kommentarer,
+                       &lt;br/&gt;kodfel, önskemål om funktioner, idéer,...</translation>
+    </message>
+    <message>
+        <source>Android app GCC offers a
+                       &lt;br/&gt;formula solver for geocaching,
+                       &lt;br/&gt;but I don&apos;t like it that much.
+                       &lt;br/&gt;Now there is a native app!</source>
+        <translation>Androidappen GCC erbjuder en
+                       &lt;br/&gt;formellösare för geocaching,
+                       &lt;br/&gt;men jag gillar den inte så mycket.
+                       &lt;br/&gt;Nu finns det en  SFOS-app!</translation>
+    </message>
+    <message>
+        <source>GMFS is packed with powerful functions.
+                       &lt;br/&gt;Check out the possibilities for importing
+                       &lt;br/&gt;a gpx file from geocaching.com
+                       &lt;br/&gt;(Premium membership only).
+                       &lt;br/&gt;Or else, just copy the geocache page
+                       &lt;br/&gt;into the import routine. I recommend
+                       &lt;br/&gt;WlanKeyboard or TinyEdit for this.
+                       &lt;br/&gt;Edit formulas and letters, and go!</source>
+        <translation>GMFS är fullspäckad med kraftfulla funktioner.
+                       &lt;br/&gt;Kolla in möjligheterna att importera
+                       &lt;br/&gt;En GPX-fil från geocaching.com
+                       &lt;br/&gt;(Endast Premium-medlemskap).
+                       &lt;br/&gt;Eller så är det bara att kopiera geocachesidan
+                       &lt;br/&gt;i importrutinen. Jag rekomenderar
+                       &lt;br/&gt;WLAN-tangentbord eller TinyEdit för detta.
+                       &lt;br/&gt;Redigera formler och bokstäver och sätt igång!</translation>
+    </message>
+    <message>
+        <source>Many calculations have been added, like
+                       &lt;br/&gt;intersections of lines and circles.
+                       &lt;br/&gt;BTW: I started using it for Mysteries as
+                       &lt;br/&gt;well, like the Bonus cache of a trail.
+                       &lt;br/&gt;Just one waypoint with the formula, and
+                       &lt;br/&gt;all letters to collect during the trail.</source>
+        <translation>Många beräkningar har lagts till, t.ex.
+                       &lt;br/&gt;skärningspunkter mellan linjer och cirklar.
+                       &lt;br/&gt;Förresten, Jag började använda det för Mysteries som
+                       &lt;br/&gt;ja, som bonuscachen på en stig.
+                       &lt;br/&gt;Bara en vägpunkt med formeln, och
+                       &lt;br/&gt;alla brev att samla in under vandringen.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>General settings</source>
+        <translation>Allmäna inställningar</translation>
+    </message>
+    <message>
+        <source>Provide Lat/Lon pattern for new waypoint</source>
+        <translation>Tillhandahåll Lat-/Long-mönster för ny vägpunkt</translation>
+    </message>
+    <message>
+        <source>Or leave formula area empty</source>
+        <translation>Eller lämna formelområdet tomt</translation>
+    </message>
+    <message>
+        <source>Copy evaluated formula for geocaching app</source>
+        <translation>Kopiera utvärderad formel för geocaching-app</translation>
+    </message>
+    <message>
+        <source>Normal behaviour: entire string, GC-mode: three parts of entire/Lat/Lon minutes</source>
+        <translation>Normalt beteende: Hela strängen, GC-läge: tre delar av hela/Lat/Long minuter</translation>
+    </message>
+    <message>
+        <source>Show hints on dialogs</source>
+        <translation>Visa tips i dialogrutor</translation>
+    </message>
+    <message>
+        <source>Explanations of entry fields</source>
+        <translation>Förklaringar till inmatningsfält</translation>
+    </message>
+    <message>
+        <source>Show Calculations in menu</source>
+        <translation>Visa beräkningar i menyn</translation>
+    </message>
+    <message>
+        <source>Partly Vincenty, partly Cartesian system used</source>
+        <translation>Delvis Vincenty, delvis kartesiskt system som används</translation>
+    </message>
+    <message>
+        <source>Use Dutch RD instead of UTM</source>
+        <translation>Använd nederländska RD istället för UTM</translation>
+    </message>
+    <message>
+        <source>RD = Rijksdriehoek system,
+Cartesian system of The Netherlands</source>
+        <translation>RD = Rijksdriehoek system,
+Det kartesiska systemet i Nederländerna</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation>Utseende</translation>
+    </message>
+    <message>
+        <source>Dark mode</source>
+        <translation>Mörkt läge</translation>
+    </message>
+    <message>
+        <source>Red characters on black background</source>
+        <translation>Röda tecken mot svart bakgrund</translation>
+    </message>
+    <message>
+        <source>Show app name on cover</source>
+        <translation>Visa appnamnet på minityren</translation>
+    </message>
+    <message>
+        <source>&apos;GMFS&apos; for better recognition of app tiles</source>
+        <translation>&quot;GMFS&quot; för bättre igenkänning av app-miniatyr</translation>
+    </message>
+    <message>
+        <source>Database actions</source>
+        <translation>Databasåtgärder</translation>
+    </message>
+    <message>
+        <source>Clear database next start-up</source>
+        <translation>Rensa databasen vid nästa uppstart</translation>
+    </message>
+    <message>
+        <source>Will be executed only once</source>
+        <translation>Kommer endast att köras en gång</translation>
+    </message>
+    <message>
+        <source>(Re-)create Multi GC3A7RC</source>
+        <translation>(Åter)skapa Multi GC3A7RC</translation>
+    </message>
+    <message>
+        <source>&apos;where it started&apos; from Seattle</source>
+        <translation>&quot;där det började&quot; från Seattle</translation>
+    </message>
+    <message>
+        <source>(Re-)create Multi GC83QV1</source>
+        <translation>(Åter)skapa Multi GC83QV1</translation>
+    </message>
+    <message>
+        <source>&apos;Het Utrechts zonnestelsel...&apos; from The Netherlands</source>
+        <translation>&quot;Het Utrechts zonnestelsel...&quot; från Nederländerna</translation>
+    </message>
+</context>
+<context>
+    <name>TextFunctions</name>
+    <message>
+        <source>This </source>
+        <translation>Denna </translation>
+    </message>
+    <message>
+        <source>waypoint is </source>
+        <translation>vägpunkt är </translation>
+    </message>
+    <message>
+        <source>geocache is </source>
+        <translation>geocache är </translation>
+    </message>
+    <message>
+        <source>Found</source>
+        <translation>Hittad</translation>
+    </message>
+    <message>
+        <source>Not Found</source>
+        <translation>Ej hittad</translation>
+    </message>
+    <message>
+        <source>GMFS</source>
+        <translation>GMFS</translation>
+    </message>
+</context>
+<context>
+    <name>TripAddPage</name>
+    <message>
+        <source>Kilometer</source>
+        <translation>Kilometer</translation>
+    </message>
+    <message>
+        <source>Date</source>
+        <translation>Datum</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>Beskrivning</translation>
+    </message>
+    <message>
+        <source>not mandatory</source>
+        <translation>Ej obligatoriskt</translation>
+    </message>
+    <message>
+        <source>Project</source>
+        <translation>Projekt</translation>
+    </message>
+</context>
+<context>
+    <name>TripShowPage</name>
+    <message>
+        <source>Add trip</source>
+        <translation>Lägg till tripp</translation>
+    </message>
+</context>
+</TS>
